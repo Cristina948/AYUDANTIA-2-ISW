399498c637e9f2622e59267c0475eaf08b389f2c
cristina moran valenzuela <cristina.moran2201@alumnos.ubiobio.cl>
2025-09-30 23:26:32 -0300

    se añadio modificar y eliminar usuario

--- src/controllers/profile.controller.js
+++ src/controllers/profile.controller.js
@@ -1,4 +1,6 @@
-import { handleSuccess } from "../Handlers/responseHandlers.js";
+import { handleSuccess, handleErrorServer, handleErrorClient} from "../Handlers/responseHandlers.js";
+import { updateUserById } from "../services/user.service.js";
+import { deleteUserById } from "../services/user.service.js";
 
 export function getPublicProfile(req, res) {
   handleSuccess(res, 200, "Perfil público obtenido exitosamente", {
@@ -13,4 +15,47 @@ export function getPrivateProfile(req, res) {
     message: `¡Hola, ${user.email}! Este es tu perfil privado. Solo tú puedes verlo.`,
     userData: user,
   });
+}
+
+//Modificar el perfil
+export async function updatePrivateProfile(req, res) {
+  try {
+    const { email, password } = req.body;
+    const userIdFromToken = req.userId; //Token del usuario
+
+    //Validación
+    if (!email && !password) {
+      return handleError(res, 400, "Debe proporcionar un email o una contraseña para actualizar.");
+    }
+    
+    //Actualizar el usuario
+    const updatedUser = await updateUserById(userIdFromToken, { email, password });
+
+    if (!updatedUser) {
+      return handleErrorClient(res, 404, "Usuario no encontrado.");
+    }
+
+    handleSuccess(res, 200, "Perfil actualizado correctamente", { user: updatedUser });
+
+  } catch (error) {
+    handleErrorServer(res, 500, "Error al actualizar el perfil.", error);
+  }
+}
+
+//Eliminar perfil
+export async function deletePrivateProfile(req, res) {
+  try {
+    const userIdFromToken = req.userId;
+
+    const isDeleted = await deleteUserById(userIdFromToken);
+
+    if (!isDeleted) {
+      return handleErrorClient(res, 404, "Usuario no encontrado o ya ha sido eliminado.");
+    }
+
+    handleSuccess(res, 200, "Perfil eliminado exitosamente.");
+    
+  } catch (error) {
+    handleErrorServer(res, 500, "Error al eliminar el perfil.", error);
+  }
 }
